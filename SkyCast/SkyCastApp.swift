b03aac9bdfc36d55fc25e20a10bce50269178c69
poojak2008 <poojabkamble20@gmail.com>
2026-07-02 16:58:47 +0530

    Initial Commit

---
+++ SkyCast/SkyCastApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkyCastApp.swift
+//  SkyCast
+//
+//  Created by pooja kamble on 02/07/26.
+//
+
+import SwiftUI
+
+@main
+struct SkyCastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
