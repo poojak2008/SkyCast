2ea93bffd29c576944f6b7ce84d2c43bb3e754fb
poojak2008 <poojabkamble20@gmail.com>
2026-07-15 22:57:44 +0530

    Commit

--- SkyCast/Views/Components/SegmentedControl.swift
+++ SkyCast/Views/Components/SegmentedControl.swift
@@ -8,11 +8,55 @@
 import SwiftUI
 
 struct SegmentedControl: View {
+    @Binding var selection: Int
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 5) {
+            HStack {
+                Button {
+                    withAnimation(.easeInOut(duration: 0.5)){
+                        selection = 0
+                    }
+                }label: {
+                        Text("Hourly Forecast")
+                    }
+                    .frame(minWidth: 0, maxWidth: .infinity )
+                Button {
+                    withAnimation(.easeInOut(duration: 0.5)){
+                        selection = 1
+                    }
+                } label: {
+                    Text("Weekly Forecast")
+                }
+                .frame(minWidth: 0,maxWidth: .infinity)
+            }
+            .font(.subheadline.weight(.semibold))
+            .foregroundColor(.secondary)
+            
+            //Mark : Separator
+            Divider()
+                .background(.white.opacity(0.5))
+                .blendMode(.overlay)
+                .shadow(color: .black.opacity(0.2), radius: 0,x: 0,y: 1)
+                .overlay{
+                    // Mark: Underline
+                    HStack {
+                        Divider()
+                            .frame(width: UIScreen.main.bounds.width / 2,height: 3)
+                            .background(Color.underline)
+                            .blendMode(.overlay)
+                    }
+                    .frame(minWidth: .infinity, alignment: selection == 0 ? .leading : .trailing)
+                    .offset(y: -1)
+                }
+        }
+        .padding(.top , 25)
     }
 }
 
-#Preview {
-    SegmentedControl()
+struct SegmentedControl_Previews: PreviewProvider {
+    static var previews: some View {
+        SegmentedControl(selection: .constant(0))
+    }
 }
+
