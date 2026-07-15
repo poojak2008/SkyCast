f3469e63a64e293882ef34339a531ce6eb1dad16
poojak2008 <poojabkamble20@gmail.com>
2026-07-15 22:57:21 +0530

    Commit

---
+++ SkyCast/Views/Components/SegmentedControl.swift
@@ -0,0 +1,18 @@
+//
+//  SegmentedControl.swift
+//  SkyCast
+//
+//  Created by pooja kamble on 12/07/26.
+//
+
+import SwiftUI
+
+struct SegmentedControl: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SegmentedControl()
+}
