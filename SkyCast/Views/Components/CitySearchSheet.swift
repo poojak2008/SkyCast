5dbfbd2f4be2275211a04d8146414c8b91a1fdff
poojak2008 <poojabkamble20@gmail.com>
2026-07-20 22:25:45 +0530

    Commit

---
+++ SkyCast/Views/Components/CitySearchSheet.swift
@@ -0,0 +1,18 @@
+//
+//  CitySearchSheet.swift
+//  SkyCast
+//
+//  Created by pooja kamble on 20/07/26.
+//
+
+import SwiftUI
+
+struct CitySearchSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CitySearchSheet()
+}
